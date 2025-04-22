edb6ecdca32437281ae41e01525624c2f69f6f4f
Ngoga Innocent Patrick <ngogainnocent1@gmail.com>
2025-04-22 09:48:02 +0200

    styling fix

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './index.html',
+      './src/**/*.{js,ts,jsx,tsx}', // Adjust according to your project structure
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
